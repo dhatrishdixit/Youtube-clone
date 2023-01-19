ddc95c1b0656583c1cb018544c6d1bf9dabbb431
Dhatrish Dixit <91131937+dhatrishdixit@users.noreply.github.com>
2023-01-19 11:02:38 +0530

    Add files via upload

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,38 @@
+// const axios = require("axios");
+// const BASE_URL='https://youtube-v31.p.rapidapi.com'
+// const options = {
+//   url: BASE_URL,
+//   params: {
+//     maxResults: '50',
+//   },
+//   headers: {
+//     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+//     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+//   }
+// };
+
+// export const fetchFromAPI = async (url) =>{
+//   const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+//   return data ;
+// }
+
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    // 'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Key': '342d029915msh26c862db09e636ep1e2affjsne72c26c9a230',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
